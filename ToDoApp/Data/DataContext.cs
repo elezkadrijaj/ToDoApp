d405dcba727cb195c0452c7834f270c7290504d2
elez-kadrijaj <150354782+elezkadrijaj@users.noreply.github.com>
2024-10-10 23:32:21 +0200

    The start of Project and connection with Database

---
+++ ToDoApp/Data/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ToDoApp.Entities;
+
+namespace ToDoApp.Data
+{
+    public class DataContext:DbContext   
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<ToDoItem> ToDoItems { get; set; }
+    }
+}
---
+++ ToDoApp/Entities/ToDoItem.cs
@@ -0,0 +1,16 @@
+﻿namespace ToDoApp.Entities
+{
+    public class ToDoItem
+    {
+        
+            public int Id { get; set; }                 // Primary key
+            public string Title { get; set; }           // Task title
+            public string Description { get; set; }     // Task description (optional)
+            public DateTime DueDate { get; set; }       // Task deadline
+            public bool IsCompleted { get; set; }       // Task completion status
+            public string Category { get; set; }        // Optional task category (e.g., Work, Personal)
+
+            
+        }
+    }
+
